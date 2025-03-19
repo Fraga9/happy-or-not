8564e702221ff19fed7b74663d31d0a93a1e88e6
Fraga9 <a00835812@tec.mx>
2025-03-19 11:27:39 -0600

    Cambios Xpresa

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkQR42ARRFOcyx2km_LEHjzqfsUfE5aEw",
-    authDomain: "happy-or-not-88f28.firebaseapp.com",
-    projectId: "happy-or-not-88f28",
-    storageBucket: "happy-or-not-88f28.firebasestorage.app",
-    messagingSenderId: "1053743740960",
-    appId: "1:1053743740960:web:3bea612481964cff13e445",    
-    measurementId: "G-Z6E2T9ZV7M"
-  };
+  apiKey: "AIzaSyAZ83MbG0Xmkt62mzrhIXfK2vk6oOtjHKE",
+  authDomain: "xpresa3.firebaseapp.com",
+  projectId: "xpresa3",
+  storageBucket: "xpresa3.firebasestorage.app",
+  messagingSenderId: "411867889667",
+  appId: "1:411867889667:web:7075f8b2337fcf98ef0e36",
+  measurementId: "G-G50SE6KZLP"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
